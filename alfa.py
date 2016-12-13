fba3ebbd1a517fe969f2c7d016674b817f94c28c
Raman Shishniou <rommer@ibuffed.com>
2016-12-13 12:57:08 +0300

    bump version 8.0.1

--- alfa.py
+++ alfa.py
@@ -58,7 +58,7 @@ class alfa:
 	os_name       = 'Android'
 	os_version    = '22'
 	device_name   = 'Android'
-	app_version   = '7.7.0'
+	app_version   = '8.0.1'
 	lang          = 'ru'
 	agent         = 'okhttp/2.6.0'
 

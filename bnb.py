91a6b558439e39b84c78eea891fba45539bce9ef
Raman Shishniou <rommer@ibuffed.com>
2017-04-08 22:21:46 +0300

    bnb.py: pep8, + raise_for_status

--- bnb.py
+++ bnb.py
@@ -10,141 +10,151 @@ import datetime
 import requests
 import time
 
+
 class bnb:
+    gate_url = 'https://bs.imbanking.by/mobile/xml_online'
+    terminal = 'Android'
 
-	gate_url = 'https://bs.imbanking.by/mobile/xml_online'
-	terminal = 'Android'
+    sessid = None
+    sess = None
 
-	sessid = None
-	sess = None
+    E = None
 
-	E = None
+    def __getstate__(self):
+        attrs = ['gate_url', 'terminal', 'sessid', 'sess']
+        return dict((attr, getattr(self, attr)) for attr in attrs)
 
-	def __getstate__(self):
-		attrs = ['gate_url', 'terminal', 'sessid', 'sess']
-		return dict((attr, getattr(self, attr)) for attr in attrs)
+    def __setstate__(self, state):
+        self.E = lxml.builder.ElementMaker()
+        for name, value in state.items():
+            setattr(self, name, value)
 
-	def __setstate__(self, state):
-		self.E = lxml.builder.ElementMaker()
-		for name, value in state.items():
-			setattr(self, name, value)
+    def __init__(self, login=None, passwd=None, sessid=None):
+        self.E = lxml.builder.ElementMaker()
 
-	def __init__(self, login = None, passwd = None, sessid = None):
+        self.sess = requests.session()
 
-		self.E = lxml.builder.ElementMaker()
+        if login is not None and passwd is not None:
+            reply = self.request(
+                'admin',
+                [
+                    self.E.Login(
+                        self.E.Parameter(login, Id='Login'),
+                        self.E.Parameter(passwd, Id='Password'),
+                        Type='PWD'
+                    ),
+                    self.E.RequestType('Login'),
+                    self.E.Subsystem('ClientAuth'),
+                ]
+            )
+            reply.raise_for_status()
+            self.sessid = str(reply.Login.SID)
 
-		self.sess = requests.session()
+        elif sessid is not None:
+            self.sessid = sessid
 
-		if login is not None and passwd is not None:
-			reply = self.request('admin', [
-				self.E.Login(
-					self.E.Parameter(login, Id = 'Login'),
-					self.E.Parameter(passwd, Id = 'Password'),
-					Type = 'PWD'
-				),
-				self.E.RequestType('Login'),
-				self.E.Subsystem('ClientAuth'),
-			])
+        else:
+            raise Exception('You must set login/passwd pair or session id')
 
-			self.sessid = str(reply.Login.SID)
+    def request(self, ext, fields=[]):
+        root = getattr(self.E, 'BS_Request')
+        tid = getattr(self.E, 'TerminalId')
+        tt = getattr(self.E, 'TerminalTime')
 
-		elif sessid is not None:
-			self.sessid = sessid
+        xml = root(
+            tid(self.terminal),
+            tt(datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
+        )
 
-		else:
-			raise Exception('You must set login/passwd pair or session id')
+        for field in fields:
+            xml.append(field)
 
-	def request(self, ext, fields = []):
+        reply = self.sess.post(
+            self.gate_url + '.' + ext,
+            data={
+                'XML': lxml.etree.tostring(xml, pretty_print=True).strip()
+            },
+            timeout=(20, 60))
 
-		root = getattr(self.E, 'BS_Request')
-		tid = getattr(self.E, 'TerminalId')
-		tt = getattr(self.E, 'TerminalTime')
+        reply.raise_for_status()
+        reply = lxml.objectify.fromstring(reply.content)
 
-		xml = root(
-			tid(self.terminal),
-			tt(datetime.datetime.now().strftime('%Y%m%d%H%M%S'))
-		)
+        errcnt = 0
+        errtxt = u'<unknown>'
 
-		for field in fields:
-			xml.append(field)
+        try:
+            errcnt = int(reply.Error.attrib.get('Count'))
+            errtxt = u' '.join(map(unicode, reply.Error.ErrorLine))
+        except:
+            pass
 
-		reply = self.sess.post(self.gate_url + '.' + ext, data = {
-			'XML': lxml.etree.tostring(xml, pretty_print=True).strip()
-		}, timeout = (20, 60))
+        if errcnt > 0:
+            errtxt = str(errcnt) + ': ' + errtxt
+            raise Exception(errtxt.encode('utf-8'))
 
-		reply = lxml.objectify.fromstring(reply.content)
+        return reply
 
-		errcnt = 0
-		errtxt = u'<unknown>'
+    def get_products(self):
+        reply = self.request(
+            'admin',
+            [
+                self.E.GetProducts(ProductType='PAY_TOOL'),
+                self.E.RequestType('GetProducts'),
+                self.E.Session(SID=self.sessid),
+                self.E.Subsystem('ClientAuth')
+            ]
+        )
 
-		try:
-			errcnt = int(reply.Error.attrib.get('Count'))
-			errtxt = u' '.join(map(unicode, reply.Error.ErrorLine))
-		except:
-			pass
+        prods = []
 
-		if errcnt > 0:
-			errtxt = str(errcnt) + ': ' + errtxt
-			raise Exception(errtxt.encode('utf-8'))
+        for p in reply.GetProducts.Product:
+            r = {}
+            for name in p.attrib:
+                r[name] = unicode(p.attrib.get(name))
+            prods.append(r)
 
-		return reply
+        return prods
 
-	def get_products(self):
+    def get_client_info(self):
+        reply = self.request(
+            'admin',
+            [
+                self.E.GetClientInfo(''),
+                self.E.RequestType('GetClientInfo'),
+                self.E.Session(SID=self.sessid),
+                self.E.Subsystem('ClientAuth')
+            ]
+        )
 
-		reply = self.request('admin', [
-			self.E.GetProducts(ProductType = 'PAY_TOOL'),
-			self.E.RequestType('GetProducts'),
-			self.E.Session(SID = self.sessid),
-			self.E.Subsystem('ClientAuth')
-		])
+        info = {}
 
-		prods = []
+        for field in reply.GetClientInfo.iter():
+            if field.tag == 'GetClientInfo':
+                continue
+            info[field.tag] = unicode(field.text)
 
-		for p in reply.GetProducts.Product:
-			r = {}
-			for name in p.attrib:
-				r[name] = unicode(p.attrib.get(name))
-			prods.append(r)
+        return info
 
-		return prods
+    def get_balance(self, clientid, product={}):
+        if 'ProductType' not in product or product['ProductType'] != 'MS':
+            raise Exception('Usupported product type')
 
-	def get_client_info(self):
+        reply = self.request(
+            'request',
+            [
+                self.E.AuthClientId(product['No'], IdType='MS'),
+                self.E.Balance(Currency=product['Currency']),
+                self.E.ClientId(clientid, IdType='Client'),
+                self.E.RequestType('Balance'),
+                self.E.Session(SID=self.sessid),
+                self.E.TerminalCapabilities(
+                    self.E.AnyAmount('Y'),
+                    self.E.ScreenWidth('62'),
+                    self.E.BooleanParameter('Y'),
+                    self.E.LongParameter('Y')
+                )
+            ]
+        )
 
-		reply = self.request('admin', [
-			self.E.GetClientInfo(''),
-			self.E.RequestType('GetClientInfo'),
-			self.E.Session(SID = self.sessid),
-			self.E.Subsystem('ClientAuth')
-		])
-
-		info = {}
-
-		for field in reply.GetClientInfo.iter():
-			if field.tag == 'GetClientInfo':
-				continue
-			info[field.tag] = unicode(field.text)
-
-		return info
-
-	def get_balance(self, clientid, product = {}):
-
-		if 'ProductType' not in product or product['ProductType'] != 'MS':
-			raise Exception('Usupported product type')
-
-		reply = self.request('request', [
-			self.E.AuthClientId(product['No'], IdType = product['ProductType']),
-			self.E.Balance(Currency = product['Currency']),
-			self.E.ClientId(clientid, IdType = 'Client'),
-			self.E.RequestType('Balance'),
-			self.E.Session(SID = self.sessid),
-			self.E.TerminalCapabilities(
-				self.E.AnyAmount('Y'),
-				self.E.ScreenWidth('62'),
-				self.E.BooleanParameter('Y'),
-				self.E.LongParameter('Y')
-			)
-		])
-
-		balance = str(reply.Balance.Amount.text)
-
-		return float(balance.replace(',', '.'))
+        balance = str(reply.Balance.Amount.text)
+        return float(balance.replace(',', '.'))

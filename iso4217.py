c17bdf08c7757622bea4f4e657d4bfb4c2de7e8a
Raman Shishniou <rommer@ibuffed.com>
2017-05-11 14:02:25 +0300

    iso4217: pep8 + add more currencies

--- iso4217.py
+++ iso4217.py
@@ -4,20 +4,25 @@
 # https://www.nbrb.by/Legislation/documents/p_33-20-44.pdf
 
 ISO4217 = {
-	'974': 'BYR',
-	'933': 'BYN',
-	'840': 'USD',
-	'978': 'EUR',
-	'643': 'RUB',
+    974: 'BYR',
+    933: 'BYN',
+    840: 'USD',
+    978: 'EUR',
+    643: 'RUB',
+    985: 'PLN',
+    756: 'CHF',
+    980: 'UAH',
+    826: 'GPB',
 }
 
 def num2name(num):
-	if str(num) in ISO4217:
-		return ISO4217[str(num)]
-	return '?'
+    num = int(num)
+    if num in ISO4217:
+        return ISO4217[num]
+    return '?'
 
 def name2num(name):
-	inv = { v: k for k, v in ISO4217.items() }
-	if name in inv:
-		return int(env[name])
-	return 0
+    inv = { v: k for k, v in ISO4217.items() }
+    if name in inv:
+        return env[name]
+    return 0

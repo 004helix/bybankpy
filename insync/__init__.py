dd51ee47a60013c56b19f6aa6676432a059296b6
Raman Shishniou <rommer@ibuffed.com>
2019-01-09 01:09:16 +0300

    insync: v2.3

--- insync/__init__.py
+++ insync/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'insync'
-__version__ = 'v2.1'
+__version__ = 'v2.3'
 
-from .v2_1 import client, InsyncException
+from .v3 import client, InsyncException
 from .history import history, InsyncHistoryException

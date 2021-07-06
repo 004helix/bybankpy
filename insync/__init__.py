88a44c75156304a4817c27064a5dd541a034bc7d
Raman Shyshniou <rommer@ibuffed.com>
2021-07-06 22:16:19 +0000

    insync: v2.26

--- insync/__init__.py
+++ insync/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'insync'
-__version__ = 'v2.22'
+__version__ = 'v2.26'
 
-from .v22 import client, InsyncException
+from .v26 import client, InsyncException
 from .history import history, InsyncHistoryException
